1a8737703b6b3ede869b6bf3a89824643209b2d9
JonsanWoon <jonsanwoon@1utar.my>
2018-08-21 11:35:16 +0800

    Snake Game (modified ver.)

---
+++ snake/Assets/mine/GameControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+	
+	public Text scoreText, gameOverText;
+	
+	int playerScore = 0;
+
+	// Use this for add score function
+	public void AddScore () {	
+		playerScore++;
+		string text = "Score: "+playerScore;
+		scoreText.text = text;
+	}
+	
+	// Game over text
+	public void GameOverText() {
+		gameOverText.enabled = true;
+		Time.timeScale = 0;
+	}
+}
