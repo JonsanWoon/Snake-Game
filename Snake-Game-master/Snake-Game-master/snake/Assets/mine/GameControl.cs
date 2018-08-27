014cc50daa94918a15d1a2e8b0a30c302ffaa420
JonsanWoon <jonsanwoon@1utar.my>
2018-08-28 03:08:13 +0800

    28/8/2018 commit

---
+++ Snake-Game-master/Snake-Game-master/snake/Assets/mine/GameControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+	
+	public Text scoreText, gameOverText, lifeText, winText;
+	
+	public int playerScore = 0;
+    int life = 3;
+
+	// Use this for add score function
+	public void AddScore () {	
+		playerScore++;
+		string text = "Score: "+playerScore;
+		scoreText.text = text;
+        //if player reach 100, player win
+        if(playerScore>=100){
+            winText.enabled = true;
+            Time.timeScale = 0;
+        }
+	}
+	
+	// Game over text
+	public void GameOverText() {
+		gameOverText.enabled = true;
+		Time.timeScale = 0;
+	}
+    
+    public bool LifeText() {
+        life--;
+        string text = "Life: "+life;
+		lifeText.text = text;
+		//Time.timeScale = 0;
+        if(life != 0){
+            return true;
+        }else{
+            return false;
+        }
+	}
+    //used in spawn.cs
+    public int getScore(){
+        return playerScore;
+    }
+}
---
+++ Snake-Game-master/Snake-Game-master/snake/Assets/mine/spawn.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+
+    public GameObject foodPrefab;
+    public GameObject powerPrefab;
+    
+    public Transform borderTop;
+    public Transform borderRight;
+    public Transform borderLeft;
+    public Transform borderBottom;
+    
+    int previousScore = 0;
+    GameControl gc;
+    bool isRefreshed = true;
+    
+	// Use this for initialization
+	void Start () {
+		Spawn(foodPrefab);
+        GameObject GameC = GameObject.Find("GameControl");
+		gc = GameC.GetComponent<GameControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int score = gc.getScore();
+        //if score not cange, refresh is true
+        if(score == previousScore){
+            isRefreshed =true;
+        }else{
+            isRefreshed =false;
+            //update score
+            previousScore = score;
+        }
+        
+        if(isRefreshed==false){
+            if(score%5 ==0){
+                refresh();
+            }
+        }
+        //spawn food
+        GameObject[] manyFood = GameObject.FindGameObjectsWithTag("food");
+		if(manyFood.Length<10){
+            Spawn(foodPrefab);
+        }
+        //spawn power
+        GameObject[] manyPower = GameObject.FindGameObjectsWithTag("power");
+		if(manyPower.Length<3){
+            Spawn(powerPrefab);
+        }
+	}
+    
+    void refresh(){
+        //destroy  all food and object
+        GameObject[] manyFood = GameObject.FindGameObjectsWithTag("food");
+        foreach(GameObject food in manyFood){
+            Destroy(food);
+        }
+        GameObject[] manyPower = GameObject.FindGameObjectsWithTag("power");
+        foreach(GameObject power in manyPower){
+            Destroy(power);
+        }
+    }
+    
+    void Spawn(GameObject prefab) {
+        float xpos;
+        float ypos;
+        float diff = 0.25f;
+        
+        //between border position [left,right->x; updown->y]
+        xpos = Random.Range(borderLeft.position.x+diff,borderRight.position.x-diff);
+        ypos = Random.Range(borderTop.position.y-diff,borderBottom.position.y+diff);
+        
+        Instantiate(prefab, new Vector2(xpos, ypos), Quaternion.identity);
+    }
+}
